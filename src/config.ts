68dfb7bb6df0a59170b4caa46c57b3fc08d5f986
achuinard <tony@twansoftware.com>
2024-05-05 17:11:36 -0400

    Updates

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,8 @@ import websiteDetails from "./website.json";
 
 export const SITE: Site = {
   NAME: `https://${websiteDetails.url}/`,
-  EMAIL: "markhorn.dev@gmail.com",
+  TITLE: websiteDetails.title,
+  DESCRIPTION: websiteDetails.description,
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -13,9 +14,4 @@ export const SITE: Site = {
 export const HOME: Metadata = {
   TITLE: "Home",
   DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
-};
-
-export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
 export type Site = {
   NAME: string;
-  EMAIL: string;
+  TITLE: string;
+  DESCRIPTION: string;
   NUM_POSTS_ON_HOMEPAGE: number;
   NUM_WORKS_ON_HOMEPAGE: number;
   NUM_PROJECTS_ON_HOMEPAGE: number;
