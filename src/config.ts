c38cca0a335146b8edaab7f80da9f66e21122a1d
achuinard <tony@twansoftware.com>
2024-05-05 17:07:46 -0400

    Test

--- src/config.ts
+++ src/config.ts
@@ -19,28 +19,3 @@ export const BLOG: Metadata = {
   TITLE: "Blog",
   DESCRIPTION: "A collection of articles on topics I am passionate about.",
 };
-
-export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
-};
-
-export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
-};
-
-export const SOCIALS: Socials = [
-  {
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
-  },
-  {
-    NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
-  },
-  {
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
-  }
-];
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,9 @@
-import { SITE } from "@config";
 import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
   type: "content",
   schema: ({ image }) =>
       z.object({
-        author: z.string().default(SITE.author),
         pubDatetime: z.date(),
         modDatetime: z.date().optional().nullable(),
         title: z.string(),
@@ -23,26 +21,4 @@ const blog = defineCollection({
       }),
 });
 
-const work = defineCollection({
-  type: "content",
-  schema: z.object({
-    company: z.string(),
-    role: z.string(),
-    dateStart: z.coerce.date(),
-    dateEnd: z.union([z.coerce.date(), z.string()]),
-  }),
-});
-
-const projects = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    date: z.coerce.date(),
-    draft: z.boolean().optional(),
-    demoURL: z.string().optional(),
-    repoURL: z.string().optional()
-  }),
-});
-
-export const collections = { blog, work, projects };
+export const collections = { blog };
