bac63f71c4ddc0b4bac4355975a465277758515a
achuinard <tony@twansoftware.com>
2024-05-05 17:25:24 -0400

    Not the best name

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-import website from 'src/website.json'
+import website from './src/website.json'
 
 export default defineConfig({
   site: website.url,
