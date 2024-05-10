86a1b385b7e6eccacb4d179e740481ee745baf39
achuinard <tony@twansoftware.com>
2024-05-09 22:56:16 -0400

    OG

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,7 +7,6 @@ const blog = defineCollection({
         pubDatetime: z.date(),
         modDatetime: z.date(),
         title: z.string(),
-        slug: z.string(),
         featured: z.boolean().optional(),
         draft: z.boolean().optional(),
         tags: z.array(z.string()).default(["others"]),
