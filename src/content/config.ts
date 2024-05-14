62fdf1c7dabd95f9db6fba18b25ea2193c2c3c97
achuinard <tony@twansoftware.com>
2024-05-13 21:06:24 -0400

    Updates

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,34 @@
-import { defineCollection, z } from "astro:content";
+import {defineCollection, z} from "astro:content";
 
 const blog = defineCollection({
-  type: "content",
-  schema: ({ image }) =>
-      z.object({
-        pubDatetime: z.date(),
-        modDatetime: z.date(),
-        title: z.string(),
-        featured: z.boolean().optional(),
-        draft: z.boolean().optional(),
-        tags: z.array(z.string()).default(["others"]),
-        ogImage: image()
-            .refine(img => img.width >= 1200 && img.height >= 630, {
-              message: "OpenGraph image must be at least 1200 X 630 pixels!",
-            })
-            .or(z.string())
-            .optional(),
-        description: z.string(),
-        canonicalURL: z.string().optional(),
-      }),
+    type: "content",
+    schema: ({image}) =>
+        z.object({
+            pubDatetime: z.date(),
+            modDatetime: z.date(),
+            title: z.string(),
+            featured: z.boolean().optional(),
+            draft: z.boolean().optional(),
+            tags: z.array(z.string()).default(["others"]),
+            ogImage: image()
+                .refine(img => img.width >= 1200 && img.height >= 630, {
+                    message: "OpenGraph image must be at least 1200 X 630 pixels!",
+                })
+                .or(z.string())
+                .optional(),
+            description: z.string(),
+            canonicalURL: z.string().optional(),
+        }),
 });
 
-export const collections = { blog };
+const pages = defineCollection({
+    type: "content",
+    schema: ({image}) =>
+        z.object({
+            pubDatetime: z.date(),
+            modDatetime: z.date(),
+            title: z.string()
+        }),
+});
+
+export const collections = {blog, pages};
